24b1c71a140c88b09bf60be27679dd545a1c0e7a
Ricky <rickycharpentier@gmail.com>
2016-11-22 18:54:09 -0500

    updated encryption

--- app/pages/encryption.py
+++ app/pages/encryption.py
@@ -4,11 +4,13 @@ import os
 
 # the block size for the cipher object; must be 16, 24, or 32 for AES
 BLOCK_SIZE = 32
-# generate a random secret key
-#secret = os.urandom(BLOCK_SIZE)
+
+# set the secret key and the IV
 secret = os.environ.get('SECRET_KEY')
+IV = BLOCK_SIZE * '\x00'
+
 # create a cipher object using the secret
-cipher = AES.new(secret)
+cipher = AES.new(secret, AES.MODE_CBC, IV)
 
 # the character used for padding--with a block cipher such as AES, the value
 # you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
@@ -25,10 +27,12 @@ DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
 
 def encode(plaintext):
     # encode a string
-    encoded = EncodeAES(cipher, plaintext)
+    #encoded = EncodeAES(cipher, plaintext)
+    encoded = cipher.encrypt(plaintext)
     return encoded
 
 def decode(encoded):
     # decode the encoded string
-    decoded = DecodeAES(cipher, encoded)
+    #decoded = DecodeAES(cipher, encoded)
+    decoded = cipher.decrypt(encoded)
     return decoded
