0d1c1abe91504736b1d604831d068222b3c5b38a
Ricky <rickycharpentier@gmail.com>
2016-11-22 18:40:13 -0500

    removed old pages

--- app/pages/oldfiles/api.py
+++
@@ -1,8 +0,0 @@
-import stripe
-
-stripe_keys = {
-    'secret_key': 'sk_test_aPce5SASjjzwyGWHlokJUOqt',
-    'publishable_key': 'pk_test_NSVlCKiuhd89Nft1EmZDivY9'
-}
-
-stripe.api_key = stripe_keys['secret_key']
