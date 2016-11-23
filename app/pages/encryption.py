f535aef24298cd47f0540be6a3260abb5ad9965d
Ricky Charpentier <rickycharpentier@gmail.com>
2016-11-23 08:20:01 -0500

    Updated encryption method

--- app/pages/encryption.py
+++ app/pages/encryption.py
@@ -8,6 +8,7 @@ BS = 16
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 unpad = lambda s : s[0:-ord(s[-1])]
 
+# AES.MODE_CBC must be multiples of 16, AES.MODE_CFB for inputs of any length
 
 class AESCipher:
 
@@ -17,13 +18,13 @@ class AESCipher:
     def encrypt( self, raw ):
         raw = pad(raw)
         iv = Random.new().read( AES.block_size )
-        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        cipher = AES.new( self.key, AES.MODE_CFB, iv )
         return base64.b64encode( iv + cipher.encrypt( raw ) )
 
     def decrypt( self, enc ):
         enc = base64.b64decode(enc + "===")
         iv = enc[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        cipher = AES.new(self.key, AES.MODE_CFB, iv )
         return unpad(cipher.decrypt( enc[16:] ))
 
 
